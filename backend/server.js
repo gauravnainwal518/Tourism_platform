da7a447b45fb7c804f187772a77cb53cbebc205a
gauravnainwal229 <gauravnainwal229@gmail.com>
2025-08-23 15:33:52 +0530

    fix regexp error

--- backend/server.js
+++ backend/server.js
@@ -50,12 +50,12 @@ app.use((req, res) => {
 });
 
 // Serve frontend in production (optional, can enable later)
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')));
-  app.get('*', (_, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
+//   app.get('*', (_, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+//   });
+// }
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
